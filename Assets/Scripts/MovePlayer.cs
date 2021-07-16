edb7a4e7cb5b3122ac8a6edccd38954569b31b38
adriandrag18 <66696874+adriandrag18@users.noreply.github.com>
2021-07-16 17:05:12 +0300

    partial

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -1,25 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    public float moveSpeed;
     public Transform camera;
+    public CapsuleCollider capsule;
+    public float groundedThreshold;
+    public float jumpSpeed;
+    public float moveSpeed;
     private Rigidbody rigidbody;
+    public float rotationSpeed;
 
-    void Start()
+    private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        capsule = GetComponent<CapsuleCollider>();
     }
 
-    void Update()
+    private void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
+        var moveDirection = GetDirection();
+        rigidbody.velocity = moveDirection * moveSpeed + Vector3.up * rigidbody.velocity.y;
 
-        Vector3 cameraForwardXoZ = Vector3.Scale(camera.forward, new Vector3(1, 0, 1));
-        Vector3 moveDirection = (cameraForwardXoZ * moveZ + camera.right * moveX).normalized;
-        rigidbody.velocity =moveDirection * moveSpeed + Vector3.up * rigidbody.velocity.y;
+        ApplyRootRotation(moveDirection);
+
+        HandleJump();
+    }
+
+    private void HandleJump()
+    {
+        Ray ray = new Ray();
+        ray.origin = transform.position + Vector3.up * groundedThreshold / 2;
+        ray.direction = Vector3.down;
+        bool grounded = false;
+
+        float maxDistance = groundedThreshold;
+        if (Physics.Raycast(ray, maxDistance))
+        {
+            grounded = true;
+            if (Input.GetButtonUp("Jump"))
+            {
+                rigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange);
+            }
+        }
+
+        Debug.DrawLine(ray.origin, ray.origin + ray.direction * maxDistance, grounded ? Color.green : Color.red);
+    }
+
+    private void ApplyRootRotation(Vector3 moveDirection)
+    {
+        if (moveDirection.magnitude < 10e-3f)
+            return;
+
+        var newRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * rotationSpeed);
+    }
+
+    private Vector3 GetDirection()
+    {
+        var moveX = Input.GetAxis("Horizontal");
+        var moveZ = Input.GetAxis("Vertical");
+
+        var cameraForwardXoZ = Vector3.Scale(camera.forward, new Vector3(1, 0, 1));
+        var moveDirection = (cameraForwardXoZ * moveZ + camera.right * moveX).normalized;
+        return moveDirection;
     }
 }
